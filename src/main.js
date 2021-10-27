9eed233374077fcde105968b324dfed9a6d472c8
nookcoder <gus5427@naver.com>
2021-10-27 18:07:23 +0900

    [ADD] vue-head 전역 설정

--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import VueHead from 'vue-head'
+
+Vue.use(VueHead)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.component('RightArrow',)
 
 Vue.config.productionTip = false
 
