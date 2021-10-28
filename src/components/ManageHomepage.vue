80d90c0f6a3fd5402aaa7863392010175137dbbb
na0 <koonayoung51@naver.com>
2021-10-28 23:51:54 +0900

    [DELETE]중복된 코드 삭제

--- src/components/ManageHomepage.vue
+++ src/components/ManageHomepage.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <h1>Hello Vu2211111111111111111111111111111111</h1>
+  <div id="manage">
+    <div class="mini-menu">
+      <ManageMiniMenu/>
+    </div>
+    <div class="edit-page">
+      <ManageEditor/>
+    </div>
   </div>
 </template>
+
+<script>
+
+import ManageEditor from './managePage/ManagePageEditor.vue'
+import ManageMiniMenu from './managePage/ManagePageMenu.vue'
+export default {
+  components: { ManageEditor, ManageMiniMenu },
+  name: 'Manage'
+}
+
+</script>
+
+<style>
+.mini-menu{
+  width: 30px;
+}
+.edit-page{
+  width:100%;
+}
+</style>
