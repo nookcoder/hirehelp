1814623a96e0dfcc35acb3db02ed2caa1d951506
nookcoder <gus5427@naver.com>
2021-10-29 16:17:16 +0900

    [ADD] store/index.js 파일 추가 ( store 관리 파일입니다 )

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Constant from '../constant';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state:{
+
+    },
+    mutations:{
+
+    },
+
+})
+
+export default store;
