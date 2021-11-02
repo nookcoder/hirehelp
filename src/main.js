963b861929c47666f8d0dcb140086feb7bc0947c
nookcoder <gus5427@naver.com>
2021-11-02 14:08:34 +0900

    [ADD] vuetify 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Router from 'vue-router'
+import vuetify from './plugins/vuetify'
 import App from './App'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
@@ -14,6 +16,8 @@ import {store} from './store'
 Vue.prototype.$eventBus = new Vue();
 Vue.prototype.$axios = axios
 
+Vue.use(vuetify)
+Vue.use(Router)
 Vue.use(VueHead)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
