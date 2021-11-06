47861e5a56d74448019142ac5032254960d1cc79
nookcoder <gus5427@naver.com>
2021-11-06 17:28:09 +0900

    [ADD] bootstrap vue header 추가

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       <router-view />
     </div>
   </div>
+  
 </template>
 
 <script>
@@ -19,12 +20,16 @@ export default {
    script:[
      {src:'https://code.jquery.com/jquery-3.5.1.min.js',async:true},
      {src:'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js',async:true},
-     {src:'https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js',async:true}
+     {src:'https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js',async:true},
+     {src:'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js',integrity:'sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx',crossorigin:'anonymous',async:true},
+     {src:'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js',integrity:'sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN',crossorigin:'anonymous',async:true},
+     {src:'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js',integrity:'sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s',crossorigin:'anonymous',async:true},
    ],
    link:[
     {href:"https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css", rel:"stylesheet"},
     {rel:'stylesheet',href:'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'},
-    {rel:'stylesheet',href:'https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css'}
+    {rel:'stylesheet',href:'https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css'},
+    {rel:'stylesheet',href:'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',integrity:'sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2',crossorigin:'anonymous'}
    ]
   }
 }
