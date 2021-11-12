80a6af9c8fc6e8e36be1f22e34400e6a50afdb1d
nookcoder <gus5427@naver.com>
2021-11-12 23:27:03 +0900

    [ADD] v-model 연결 in Apply.vue

--- src/views/components/homepage/Apply.vue
+++ src/views/components/homepage/Apply.vue
@@ -4,29 +4,70 @@
             <div class="personal-information">
                 <div class="apply-column-title">인적사항</div>
                 <div class="apply-column-input">
-                    <b-form-input class="apply-column-personal-input" placeholder="성명" required></b-form-input>
-                    <b-form-datepicker class="apply-column-personal-input" placeholder="생년월일" required></b-form-datepicker>
-                    <b-form-input class="apply-column-personal-input" list="gender" placeholder="성별" ></b-form-input>
+                    <b-form-input 
+                        class="apply-column-personal-input"
+                        placeholder="성명"
+                        required
+                        v-model="applyData.name"
+                        ></b-form-input>
+                    
+                    <b-form-datepicker
+                        class="apply-column-personal-input" 
+                        placeholder="생년월일" 
+                        v-model="applyData.birth"
+                        required
+                        ></b-form-datepicker>
+                    
+                    <b-form-input 
+                        class="apply-column-personal-input" 
+                        list="gender" 
+                        placeholder="성별"
+                        required
+                        v-model="applyData.gender"
+                         ></b-form-input>
                     <datalist id="gender">
                         <option v-for="gender in gender" :key=gender>{{gender}}</option>
                     </datalist>
                 </div>
+
                 <div class="apply-column-input">
-                    <b-form-input class="apply-column-personal-input" placeholder="이메일 ID"></b-form-input>
+                    <b-form-input 
+                        class="apply-column-personal-input" 
+                        placeholder="이메일 ID"
+                        required
+                        v-model="applyData.email"
+                        ></b-form-input>
                     <div id="dot">@</div>
-                    <b-form-input class="apply-column-personal-input" placeholder="이메일 주소"></b-form-input>
+                    <b-form-input 
+                        class="apply-column-personal-input" 
+                        placeholder="이메일 주소"
+                        required
+                        v-model="applyData.emailAddress"
+                        ></b-form-input>
                 </div>
                 <div class="apply-column-input">
-                    <b-form-input class="apply-column-personal-input" placeholder="휴대폰 번호(- 제외)"></b-form-input>
-                    <b-form-input class="apply-column-personal-input" placeholder="전화번호(- 제외)"></b-form-input>
+                    <b-form-input 
+                        class="apply-column-personal-input" 
+                        placeholder="휴대폰 번호(- 제외)"
+                        v-model="applyData.phoneNumber"
+                        required
+                        ></b-form-input>
+                    <b-form-input 
+                        class="apply-column-personal-input" 
+                        placeholder="전화번호(- 제외)"
+                        required
+                        v-model="applyData.phoneNumber2"
+                        ></b-form-input>
                 </div>
             </div>
-            <div class="motiive">
+            <div class="motivate">
                 <div class="apply-column-title">지원동기</div>
                 <b-form-textarea
                     class="apply-column-personal-input"
                     placeholder="내용을 입력해주시기 바랍니다."
                     rows="10"
+                    required
+                    v-model="applyData.motivate"
                 ></b-form-textarea>
             </div>
             <div class="attachment">
@@ -39,8 +80,8 @@
                 ></b-form-file>
             </div>
 
-            <b-button>제출하기</b-button>
-            <b-button>뒤로가기</b-button>
+            <b-button type="submit" variant="primary">제출하기</b-button>
+            <b-button variant="danger" @click="$router.go(-1)">뒤로가기</b-button>
         </form>
     </div>
 </template>
@@ -50,6 +91,23 @@ export default {
     data(){
         return{
             gender:["남자","여자"],
+            applyData:{
+                name:'',
+                birth:'',
+                gender:'',
+                email:'',
+                emailAddress:'',
+                phoneNumber:'',
+                phoneNumber2:'',
+                motivate:'',
+            },
+            
+        }
+    },
+    methods:{
+        onSubmit:function(event){
+            event.preventDefault();
+            
         }
     }
 }
