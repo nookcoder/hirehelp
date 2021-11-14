57ba1627386c231cfa3f87c38a0c406177164621
na0 <koonayoung51@naver.com>
2021-11-14 18:27:41 +0900

    [ADD] 로그인 시, 토큰 발급 받고 헤더에 저장

--- src/store.js
+++ src/store.js
@@ -1,15 +1,76 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import router from './router';
 
 Vue.use(Vuex);
 
-export const store = new Vuex.Store({
-    state:{
-        currentUser:'null',
+export default new Vuex.Store({
+    state: {
+        host: 'http://localhost:3000',
+        currentUser: [],
+        isLogin: false,
+        isloginError: false
     },
-    mutations:{ 
+    mutations: {
+        // 로그인 성공
+        loginSuccess(state, payload) {
+            state.isLogin = true
+            state.isloginError = false
+            state.currentUser = payload
+        },
+        // 로그인 실패
+       loginError(state) {
+            state.isLogin = false
+            state.isloginError = true
+            state.currentUser = null
+        },
+        logout(state) {
+            state.isLogin = false
+            state.isloginError = false
+            state.currentUser = null
+       },
         setCurrentUser: function(state,userId){
             state.currentUser = userId;
+        }
+    },
+    actions: {
+        // 로그인 시도
+        loginStore({ commit }, loginForm) {
+            axios.post('http://localhost:3000/api/company/login', loginForm)
+            .then(res => {
+                alert(res.data.message);
+                if (res.data.loginSuccess) {                    
+                    let config = {
+                        headers: { "access-token": res.data.accessToken }
+                    }
+                        axios.get('http://localhost:3000/api/company/login', config)
+                        .then(response => {
+                        let selectedUser = {
+                            id: response.data.token.id,
+                            name: response.data.token.name
+                        }
+                        commit("loginSuccess", selectedUser)
+                        router.push('/form/notice') 
+                        })
+                        .catch(error => {
+                        console.log(error)
+                        })
+                // 로그인 성공 $store.dispatch("loginStore")
+                }
+                else {
+                // 로그인 실패
+                    commit("loginError")
+                }
+                  
+            }).catch(err => {
+                console.log(err)
+                commit("loginError")
+            });
+        },
+        logoutStore({ commit }) {
+            commit("logout")
+            router.push('/')
         }
     },
     getters:{
