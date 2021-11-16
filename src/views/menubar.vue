252eafe0de4c82787d86c514c4beef0df69af203
na0 <koonayoung51@naver.com>
2021-11-17 02:32:02 +0900

    [FIX] merge 후, 로그아웃 등 안되던 오류 수정

--- src/views/menubar.vue
+++ src/views/menubar.vue
@@ -1,12 +1,13 @@
 <template>
 <div>
   <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand @click="$router.push('/main')">HOME</b-navbar-brand>
+    <div class="ml-auto order-0">
+       <b-navbar-brand class="ml-auto" @click="$router.push('/main')">HOME</b-navbar-brand>
+    </div>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
+      <b-navbar-nav class="me-auto">
         <b-nav-dropdown text="홈페이지 관리">
             <b-dropdown-item @click="$router.push('notice')">공지사항 추가</b-dropdown-item>
             <b-dropdown-item>공지사항 수정/삭제</b-dropdown-item>
@@ -24,17 +25,21 @@
         <b-nav-item @click="moveUserHomePage">채용 홈페이지 접속</b-nav-item>
       </b-navbar-nav>
 
-      <b-navbar-nav class="ml-auto">
+       <b-navbar-nav class="ml-auto">
         <div v-if="this.$store.state.isLogin">
-        <b-nav-item v-on:click="logoutStore()">{{this.user.name}} 로그아웃</b-nav-item>
+          <b-collapse id="nav-collapse" is-nav>
+        <b-nav-item @click="$router.push('mypage')">{{this.$store.state.currentUser.name}} 님</b-nav-item>
+        <b-nav-item >|</b-nav-item>
+        <b-nav-item v-on:click="logoutStore()">로그아웃</b-nav-item>
+          </b-collapse>
         </div>
         <div v-else>
             <b-nav-item-dropdown right>
           <template #button-content>
             <b-icon icon="person-fill"></b-icon>
           </template>
-          <b-dropdown-item @click="$router.push('login')">로그인</b-dropdown-item>
-          <b-dropdown-item @click="$router.push('signup')">회원가입</b-dropdown-item>
+          <b-dropdown-item @click="$router.push('/login')">로그인</b-dropdown-item>
+          <b-dropdown-item @click="$router.push('/signup')">회원가입</b-dropdown-item>
         </b-nav-item-dropdown>
         </div>
       </b-navbar-nav>
@@ -50,11 +55,11 @@ export default {
     name:"Menu",
     data(){
       return{
-        user:this.$store.getters.getcurrentUser
+        user:this.$store.state.currentUser
       }
     },
     computed:{
-      ...mapState(["isLogin"])
+      ...mapState(["isLogin", "isloginError"])
     },
     methods:{
       ...mapActions(["logoutStore"]),
@@ -66,5 +71,5 @@ export default {
 </script>
 
 <style>
-
+@import "../assets/styles/reset.css";
 </style>
