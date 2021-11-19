d21012bd603adccec7e3a359f1affffbc8ab177f
na0 <koonayoung51@naver.com>
2021-11-20 06:01:29 +0900

    [ADD] 지훈 오빠 서버 파스타에 연결해서 라우트 링크 바꿈

--- src/views/menubar.vue
+++ src/views/menubar.vue
@@ -20,7 +20,6 @@
         <b-nav-dropdown text="면접 서비스">
             <b-dropdown-item @click="onClickRedirect()">면접실 입장</b-dropdown-item>
             <b-dropdown-item @click="$router.push('/interviewsetting')">면접시간 배정</b-dropdown-item>
-            <b-dropdown-item>면접평가서 등록 및 수정</b-dropdown-item>
         </b-nav-dropdown>
         <b-nav-item @click="moveUserHomePage">채용 홈페이지 접속</b-nav-item>
       </b-navbar-nav>
@@ -62,7 +61,7 @@ export default {
     },
     methods:{
       onClickRedirect(){
-          window.open("https://interview.loca.lt")
+          window.open("https://interview.paas-ta.org")
       },
       ...mapActions(["logoutStore"]),
       moveUserHomePage(){
