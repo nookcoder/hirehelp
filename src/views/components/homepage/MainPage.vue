55e60edb4d951a5f73fa2e1393f854e03d8d91c8
nookcoder <gus5427@naver.com>
2021-11-19 22:18:26 +0900

    [ADD] 회사 소개 제목 내용 메인 페이지에 뜨도록 수정

--- src/views/components/homepage/MainPage.vue
+++ src/views/components/homepage/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="jumbotron">
-        <h1 class="display-4">{{this.$store.state.currentUser.introduceTitle}}</h1>
-        <p class="lead">{{this.$store.state.currentUser.introduce}}</p>
+        <h1 class="display-4">{{companyData.company_introduce_title}}</h1>
+        <p class="lead">{{companyData.company_introduce}}</p>
         <router-link class="btn btn-primary btn-lg" role="button" :to="'/home/' + this.$store.state.currentUser.id+'/employment'">채용 공고 바로 가기</router-link>
     </div>
 </template>
