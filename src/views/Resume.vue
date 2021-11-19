b0ad1f806028434a1a2ad0f88546e546e36e8571
na0 <koonayoung51@naver.com>
2021-11-19 18:43:40 +0900

    외않되

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,35 +1,6 @@
 <template>
 	<v-container>
 		<v-card elevation="10" outlined width="100%" class="mx-auto">
-			<!-- <v-card-title>
-			<v-card-title>
-				이력서 관리
-			</v-card-title>
-			<v-card-text>
-      <div class="resume-page">
-        <b-tabs card>
-          <b-tab title="접수중">
-            <b-table 
-              responsive 
-              flex 
-              style="width: 100%; max-height: 70vh;" 
-              striped 
-              hover 
-              :items="jobPostings" 
-              :fields="fields"
-              >
-              <template  v-slot:cell(driver)="data">
-                <b-button size="sm" @click="loadApplicantsData(data)" class="mr-1">Info</b-button>
-              </template>
-            </b-table>
-            <div class="pagination-div">
-            </div>
-          </b-tab>
-        <b-tab title="접수마감">
-          <b-table responsive flex style="width: 100%; max-height: 70vh;" @click="tableClick" striped hover :items="endjobPostings" :fields="endFields">
-    </b-table>
-          <b-table responsive flex style="width: 100%; max-height: 70vh;" striped hover :items="endjobPostings" :fields="endFields">
-    </b-table> -->
 			<v-card-title>
 				이력서 관리
 			</v-card-title>
@@ -62,7 +33,7 @@
             :items="endjobPostings" 
             :fields="endFields">
           </b-table>
-      </b-tab>
+        </b-tab>
       </b-tabs>
   </div>
       </v-card-text>
@@ -75,7 +46,6 @@
 export default {
   data(){
     return{
-      tabIndex: 0,
       jobPostings:[],
       endjobPostings:[],
       endFields: [
@@ -143,10 +113,6 @@ export default {
           key: 'driver',
           label: '현재 지원자 수',
           sortable: true
-        },
-        {
-          key: 'actions',
-          label: '버튼',
         }
       ]
     }
@@ -164,7 +130,8 @@ export default {
         var result = Math.ceil(((new Date(Response.data[post].end_date)) - dday) / (1000 * 60 * 60 * 24));
         if(result > 0){
           Response.data[post].Dday = result
-          this.jobPostings.push(Response.data[post])}
+          this.jobPostings.push(Response.data[post])
+        }
         else{
           this.endjobPostings.push(Response.data[post])
         }
@@ -174,13 +141,13 @@ export default {
     console.log(Error);
     })
     },
-  },mounted: {
-		// this.getRecruitmentList();
     loadApplicantsData: function(data){
       this.$router.replace("resume/"+data.item.company_id+"/"+data.item.id);
-    }
-	},computed(){
-  }
+    },
+  },
+  mounted() {
+		this.getRecruitmentList();
+	}
 }
 </script>
 
