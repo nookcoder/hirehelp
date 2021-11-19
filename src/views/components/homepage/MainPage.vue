0f39fc1ee0049a253107b1f5001c9589c7312c38
nookcoder <gus5427@naver.com>
2021-11-19 22:21:15 +0900

    [FIX] 채용공고 바로가기 버튼 기능 수정

--- src/views/components/homepage/MainPage.vue
+++ src/views/components/homepage/MainPage.vue
@@ -2,7 +2,7 @@
    <div class="jumbotron">
         <h1 class="display-4">{{companyData.company_introduce_title}}</h1>
         <p class="lead">{{companyData.company_introduce}}</p>
-        <router-link class="btn btn-primary btn-lg" role="button" :to="'/home/' + this.$store.state.currentUser.id+'/employment'">채용 공고 바로 가기</router-link>
+        <b-button class="btn btn-primary btn-lg" @click="$router.push($route.params.id + '/employment')">채용 공고 바로 가기</b-button>
     </div>
 </template>
 
